f5c1bb899e1747fd7d9c90d64019bff25610d5ce
Akbarsn <new.akbarsatya@gmail.com>
2019-04-13 17:48:40 +0700

    Creating front end

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -17,7 +17,7 @@
         $(document).ready(function () {
             var auto_refresh = setInterval(
                 function () {
-                    $('#showdata').load('/admin/product').fadeIn("slow");
+                    $('#showdataall').load('/admin/product').fadeIn("slow");
                 }, 100);
         })
 
@@ -53,10 +53,10 @@
                 <div class="container">
                     <h4>Filter</h4>
                     <div class="list-group list-group-flush">
-                        <a href="#" class="list-group-item list-group-item-action">Cras justo odio</a>
-                        <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-                        <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-                        <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+                        <a href="#" class="list-group-item list-group-item-action">Diet Harian</a>
+                        <a href="#" class="list-group-item list-group-item-action">Diet Khusus</a>
+                        <a href="#" class="list-group-item list-group-item-action">Single Lunch Box</a>
+                        <a href="#" class="list-group-item list-group-item-action">Diet Penurunan Berat Badan</a>
                     </div>
                 </div>
             </aside>
@@ -64,29 +64,134 @@
 
         <div class="col-lg-6" id="main-content">
             <div class="container">
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th rowspan="2" scope="col">ID</th>
-                            <th colspan="2" scope="col">Product</th>
-                            <th colspan="3" scope="col">User</th>
-                            <th rowspan="2" scope="col">Invoice</th>
-                            <th rowspan="2" scope="col">Receipt</th>
-                            <th rowspan="2" scope="col">Times</th>
-                            <th rowspan="2" scope="col">Status</th>
-                        </tr>
-                        <tr>
-                            <th>Product Name</th>
-                            <th>Product Price</th>
-                            <th>User Name</th>
-                            <th>User Phone</th>
-                            <th>User Address</th>
-                        <tr>
-                    </thead>
-                    <tbody id="showdata">
+                <h3>Today Batch</h3>
+                <h6>Kuota yang harus dikirim hari ini</h6>
+                <div class="changeable">
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#all" role="tab"
+                                aria-controls="home" aria-selected="true">All</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#unpaid" role="tab"
+                                aria-controls="profile" aria-selected="false">Unpaid</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#paid" role="tab"
+                                aria-controls="contact" aria-selected="false">Paid</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#done" role="tab"
+                                aria-controls="contact" aria-selected="false">Done</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="home-tab">
+                            <br>
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th rowspan="2" scope="col">ID</th>
+                                        <th colspan="2" scope="col">Product</th>
+                                        <th colspan="3" scope="col">User</th>
+                                        <th rowspan="2" scope="col">Invoice</th>
+                                        <th rowspan="2" scope="col">Receipt</th>
+                                        <th rowspan="2" scope="col">Times</th>
+                                        <th rowspan="2" scope="col">Status</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Product Name</th>
+                                        <th>Product Price</th>
+                                        <th>User Name</th>
+                                        <th>User Phone</th>
+                                        <th>User Address</th>
+                                    <tr>
+                                </thead>
+                                <tbody id="showdataall">
 
-                    </tbody>
-                </table>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="tab-pane fade" id="unpaid" role="tabpanel" aria-labelledby="profile-tab">
+                            <br>
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th rowspan="2" scope="col">ID</th>
+                                        <th colspan="2" scope="col">Product</th>
+                                        <th colspan="3" scope="col">User</th>
+                                        <th rowspan="2" scope="col">Invoice</th>
+                                        <th rowspan="2" scope="col">Receipt</th>
+                                        <th rowspan="2" scope="col">Times</th>
+                                        <th rowspan="2" scope="col">Status</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Product Name</th>
+                                        <th>Product Price</th>
+                                        <th>User Name</th>
+                                        <th>User Phone</th>
+                                        <th>User Address</th>
+                                    <tr>
+                                </thead>
+                                <tbody id="showdataunpaid">
+
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="tab-pane fade" id="paid" role="tabpanel" aria-labelledby="contact-tab">
+                            <br>
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th rowspan="2" scope="col">ID</th>
+                                        <th colspan="2" scope="col">Product</th>
+                                        <th colspan="3" scope="col">User</th>
+                                        <th rowspan="2" scope="col">Invoice</th>
+                                        <th rowspan="2" scope="col">Receipt</th>
+                                        <th rowspan="2" scope="col">Times</th>
+                                        <th rowspan="2" scope="col">Status</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Product Name</th>
+                                        <th>Product Price</th>
+                                        <th>User Name</th>
+                                        <th>User Phone</th>
+                                        <th>User Address</th>
+                                    <tr>
+                                </thead>
+                                <tbody id="showdatapaid">
+
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="tab-pane fade" id="done" role="tabpanel" aria-labelledby="contact-tab">
+                            <br>
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th rowspan="2" scope="col">ID</th>
+                                        <th colspan="2" scope="col">Product</th>
+                                        <th colspan="3" scope="col">User</th>
+                                        <th rowspan="2" scope="col">Invoice</th>
+                                        <th rowspan="2" scope="col">Receipt</th>
+                                        <th rowspan="2" scope="col">Times</th>
+                                        <th rowspan="2" scope="col">Status</th>
+                                    </tr>
+                                    <tr>
+                                        <th>Product Name</th>
+                                        <th>Product Price</th>
+                                        <th>User Name</th>
+                                        <th>User Phone</th>
+                                        <th>User Address</th>
+                                    <tr>
+                                </thead>
+                                <tbody id="showdatadone">
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
