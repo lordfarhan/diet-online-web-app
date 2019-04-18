a4f5a66aeb54de7c923d9cb5cb80d087928b7627
Akbarsn <new.akbarsatya@gmail.com>
2019-04-19 00:12:30 +0700

    Fixed API

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -10,57 +10,58 @@ class APIController extends Controller
 {
     public function InsertTransactions(request $request)
     {
-        $invoice = uniqid("INV",false);
+        $invoice = uniqid("INV", false);
         $user_id = $request->user_id;
         $product_id = $request->product_id;
         $pilihan_hari = $request->days;
         $pilihan_waktu = $request->times;
         $banyak_porsi = $request->jumlah;
+        $notes = $request->notes;
         $date = date('d');
         $hari = date('N') - 1;
         $bulan = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
         $bulanSekarang = date('n');
 
-        $pecahanwaktu = ($pilihan_waktu == 1 || $pilihan_waktu == 2 || $pilihan_waktu == 3) ? $pilihan_waktu : explode("-", $pilihan_waktu);
+        $pecahanwaktu = [0, 0, 0];
         $booleanwaktu = [0, 0, 0];
-        echo implode("-",$pecahanwaktu);
-
         for ($i = 0; $i < 3; $i++) {
             if (is_array($pecahanwaktu)) {
-                if ($pecahanwaktu[i] == 0) {
+                $pecahanwaktu[$i] = \substr($pilihan_waktu, $i, 1);
+                if ($pecahanwaktu[$i] == 1) {
                     $booleanwaktu[0] = 1;
-                } else if ($pecahanwaktu[i] == 1) {
+                } else if ($pecahanwaktu[$i] == 2) {
                     $booleanwaktu[1] = 1;
-                } else if ($pecahanwaktu[i] == 2) {
+                } else if ($pecahanwaktu[$i] == 3) {
                     $booleanwaktu[2] = 1;
                 }
-            } else if ($i === $pecahanwaktu - 1) {
-                $booleanwaktu[$pecahanwaktu - 1] = 1;
+            } else {
+                $booleanwaktu[$pilihan_waktu - 1] = 1;
             }
         }
 
-        $pecahanhari = ($pilihan_hari == 1 || $pilihan_hari == 2 || $pilihan_hari == 3 || $pilihan_hari == 4 || $pilihan_hari == 5 || $pilihan_hari == 6 || $pilihan_hari == 7) ? $pilihan_hari : explode("-", $pilihan_hari);
+        $pecahanhari = [0, 0, 0, 0, 0, 0, 0];
         $booleanhari = [0, 0, 0, 0, 0, 0, 0];
 
         for ($i = 0; $i < 7; $i++) {
             if (is_array($pecahanhari)) {
-                if ($pecahanhari[$i] == 0) {
+                $pecahanhari[$i] = \substr($pilihan_hari, $i, 1);
+                if ($pecahanhari[$i] == 1) {
                     $booleanhari[0] = 1;
-                } else if ($pecahanhari[$i] == 1) {
-                    $booleanhari[1] = 1;
                 } else if ($pecahanhari[$i] == 2) {
-                    $booleanhari[2] = 1;
+                    $booleanhari[1] = 1;
                 } else if ($pecahanhari[$i] == 3) {
-                    $booleanhari[3] = 1;
+                    $booleanhari[2] = 1;
                 } else if ($pecahanhari[$i] == 4) {
-                    $booleanhari[4] = 1;
+                    $booleanhari[3] = 1;
                 } else if ($pecahanhari[$i] == 5) {
-                    $booleanhari[5] = 1;
+                    $booleanhari[4] = 1;
                 } else if ($pecahanhari[$i] == 6) {
+                    $booleanhari[5] = 1;
+                } else if ($pecahanhari[$i] == 7) {
                     $booleanhari[6] = 1;
                 }
-            } else if ($i == $pecahanhari - 1) {
-                $booleanhari[$pecahanhari - 1] = 1;
+            } else {
+                $booleanhari[$pilihan_hari - 1] = 1;
             }
         }
 
@@ -78,37 +79,35 @@ class APIController extends Controller
                     if ($tanggalPesanan > (365 - date('z'))) {
                         $tahunPesanan += 1;
                         $bulanPesanan = 1;
-                        if ($tanggalPesanan > $bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1] + $bulan[$bulanPesanan + 1] + $bulan[$bulanPesanan + 2]) {
-                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1] + $bulan[$bulanPesanan + 1] + $bulan[$bulanPesanan + 2]);
+                        $tanggalPesanan -= 365 - date('z');
+                        if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1] + $bulan[$bulanSekarang + 2]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1] + $bulan[$bulanSekarang + 2]);
                             $bulanPesanan = $bulanSekarang + 4;
-                        } else if ($tanggalPesanan > $bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1] + $bulan[$bulanPesanan + 1]) {
-                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1] + $bulan[$bulanPesanan + 1]);
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1]);
                             $bulanPesanan = $bulanSekarang + 3;
-                        } else if ($tanggalPesanan > $bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1]) {
-                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1]);
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1]);
                             $bulanPesanan = $bulanSekarang + 2;
-                        } else if ($tanggalPesanan > $bulan[$bulanPesanan - 1]) {
-                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanPesanan - 1]);
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang - 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang - 1]);
                             $bulanPesanan = $bulanSekarang + 1;
                         } else {
                             $bulanPesanan = $bulanSekarang;
                         }
-                        $tanggalPesanan -= 365 - date('z');
                     } else {
-                        if ($tanggalPesanan > $bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1] + $bulan[$bulanPesanan + 1] + $bulan[$bulanPesanan + 2]) {
-                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1] + $bulan[$bulanPesanan + 1] + $bulan[$bulanPesanan + 2]);
+                        if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1] + $bulan[$bulanSekarang + 2]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1] + $bulan[$bulanSekarang + 2]);
                             $bulanPesanan = $bulanSekarang + 4;
-                        } else if ($tanggalPesanan > $bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1] + $bulan[$bulanPesanan + 1]) {
-                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1] + $bulan[$bulanPesanan + 1]);
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1]);
                             $bulanPesanan = $bulanSekarang + 3;
-                        } else if ($tanggalPesanan > $bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1]) {
-                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanPesanan] + $bulan[$bulanPesanan - 1]);
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1]);
                             $bulanPesanan = $bulanSekarang + 2;
-                        } else if ($tanggalPesanan > $bulan[$bulanPesanan - 1]) {
-                            // echo $tanggalPesanan;
-                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanPesanan - 1]);
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang - 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang - 1]);
                             $bulanPesanan = $bulanSekarang + 1;
-                            // echo "masuk";
                         } else {
                             $bulanPesanan = $bulanSekarang;
                         }
@@ -123,36 +122,45 @@ class APIController extends Controller
                         break;
                     }
                     if ($booleanhari[$i] == 1 && $booleanwaktu[$j] == 1 && $banyak_porsi != 0) {
-                        $dateInput = $tanggalPesanan . "-" . $bulanPesanan . "-" . $tahunPesanan;
-                        $transaction = new Transaction;
-                        // echo $dateInput;
-                        $transaction->invoice = $invoice;
-                        $transaction->user_id = $user_id;
-                        $transaction->product_id = $product_id;
-                        $transaction->notes = "  ";
-                        $transaction->proof_of_payment = "  ";
-                        $transaction->date = Carbon::createFromFormat('d-m-Y', $dateInput)->toDateString();
-                        $transaction->times = $j + 1;
-                        $transaction->status = 1;
-                        $transaction->save();
-                        $banyak_porsi--;
+                        try {
+                            $dateInput = $tanggalPesanan . "-" . $bulanPesanan . "-" . $tahunPesanan;
+                            $transaction = new Transaction;
+                            $transaction->invoice = $invoice;
+                            $transaction->user_id = $user_id;
+                            $transaction->product_id = $product_id;
+                            $transaction->notes = $notes;
+                            $transaction->proof_of_payment = "  ";
+                            $transaction->date = Carbon::createFromFormat('d-m-Y', $dateInput)->toDateString();
+                            $transaction->times = $j + 1;
+                            $transaction->status = 1;
+                            $transaction->save();
+                            return response()->json(["Message" => "Succesful"]);
+                            $banyak_porsi--;
+                        } catch (Error $error) {
+                            return response()->json(["Message" => "Unsuccesful", "Error" => $error]);
+                        }
                     }
                 }
             }
         }
     }
-    public function UpdateStatus(Request $request){
-        $invoice = $request->invoice; 
+    public function UpdateStatus(Request $request)
+    {
+        $invoice = $request->invoice;
         $proof = $request->proof_of_payment;
-        $hours = date('H');
-        if($hours>17){
-            $transaction = Transaction::find($invoice);
-            $transaction->delete();
-        } else {
-            $transaction = Transaction::find($invoice);
-            $transaction->status = 1;
-            $transaction->proof_of_payment = $proof;
-            $transaction->save();
+        $hours = (date('H') + 7) % 24;
+        $hours = 18;
+        try {
+            if ($hours > 17) {
+                $transactions = Transaction::where('invoice', $invoice)->get(['id']);
+                Transaction::destroy($transactions->toArray());
+                return response()->json(["Message" => "Deleted"]);
+            } else {
+                $transactions = Transaction::select()->where('invoice', '=', $invoice)->update(['status' => 1, 'proof_of_payment' => $proof]);
+                return response()->json(["Message" => "Updated"]);
+            }
+        } catch (Error $error) {
+            return response()->json(["Message" => "Unsuccesful", "Error" => $error]);
         }
     }
 }
