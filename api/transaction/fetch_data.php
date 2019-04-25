59ffa63f7fdc1b2389f23dc9f30d6f31d3faadb7
Akbarsn <new.akbarsatya@gmail.com>
2019-04-25 23:30:20 +0700

    Added Fetch Data API

---
+++ api/transaction/fetch_data.php
@@ -0,0 +1,18 @@
+<?php
+define('root', $_SERVER['DOCUMENT_ROOT']);
+require_once(root . '/diet-online-apps-web/api/function/TransactionFunction.php');
+
+$db = new TransactionFunction();
+$response['error'] = false;
+$user_id = $_REQUEST['user_id'];
+$status = $_REQUEST['status'];
+$result = $db->FetchData($user_id,$status);
+if($result!=FALSE){
+    $response['message']= "Fetch Success";
+    $response['transaction']=$result;
+    echo json_encode($response);
+} else {
+    $response['error']= true;
+    $response['message']="Fetching data Failed";
+    echo json_encode($response);
+}
