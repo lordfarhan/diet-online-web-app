afa8f221f1d1cd0a66e4731bb6db75fca7cb36ef
Akbarsn <new.akbarsatya@gmail.com>
2019-05-15 13:46:36 +0700

    Added Post for Diet Khusus

--- api/function/TransactionFunction.php
+++ api/function/TransactionFunction.php
@@ -361,10 +361,10 @@ class TransactionFunction
         $tahunPesanan = $tahunSekarang;
         for ($i = 0; $i < 13; $i++) {
             $check = false;
-            if ($bulanSekarang == 11 && $tanggalPesanan + 1 > $bulan[$bulanSekarang-1]) {
+            if ($bulanSekarang == 11 && $tanggalPesanan + 1 > $bulan[$bulanSekarang - 1]) {
                 $tahunPesanan++;
                 $bulanPesanan = 0;
-            } else if ($tanggalPesanan + 1 > $bulan[$bulanSekarang-1]) {
+            } else if ($tanggalPesanan + 1 > $bulan[$bulanSekarang - 1]) {
                 $bulanPesanan++;
             }
             $dateInput = $tanggalPesanan . "-" . $bulanPesanan . "-" . $tahunPesanan;
@@ -422,8 +422,249 @@ class TransactionFunction
         }
     }
 
-    public function DietKhusus($user_id, $product_id, $notes)
-    { }
+    public function DietKhusus($user_id, $product_id, $days, $times, $amount, $notes, $activity)
+    {
+        //Activity 
+        //0= No exercise, 1=Light, 2=Moderate, 3= Heavy, 4=Very Heavy
+        $invoice = uniqid("INV", false);
+        $date = date('d');
+        $hari = date('N') - 1;
+        $bulan = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        $bulanSekarang = date('n');
+        $tahunSekarang = date('Y');
+        $user = $this->GetUser($user_id);
+        $weight = $user['weight'];
+        $height = $user['height'];
+        $gender = $user['gender']; //0 = Laki Laki, 1 = Perempuan
+        $birth_date = $user['birth_date'];
+        $splitBirthDate = explode('-', $birth_date);
+        $age = $tahunSekarang - $splitBirthDate[0];
+        $statusGizi = 0; //1=Kurus, 2=Normal, 3=Overweight, 4=Obesitas
+        $bmi = $weight / (($height / 100.0) * ($height / 100.0));
+        $adjustedWeight = 0;
+        $bmr = 0;
+        $dailyCalories = 0;
+        if ($bmi < 18.5) {
+            $statusGizi = 1;
+        } else if ($bmi < 25.1) {
+            $statusGizi = 2;
+        } else if ($bmi < 27.1) {
+            $statusGizi = 3;
+        } else {
+            $statusGizi = 4;
+        }
+        switch ($statusGizi) {
+            case 1 || 2:
+                $adjustedWeight = $weight;
+                break;
+            case 3:
+                $adjustedWeight = ($height - 100) - (0.1 * ($height - 100));
+                break;
+            case 4:
+                $adjustedWeight = $weight - (($height - 100) - (0.1 * ($height - 100)) * 0.25) + ($height - 100) - (0.1 * ($height - 100));
+                break;
+        }
+        //BMR
+        switch ($gender) {
+            case 0:
+                $bmr = 66 + (13.7 * $adjustedWeight) + (5 * $height) - (6.76 * $age);
+                break;
+            case 1:
+                $bmr = 655 + (9.6 * $adjustedWeight) + (1.8 * $height) - (4.7 * $age);
+                break;
+        }
+
+        switch ($activity) {
+            case 0:
+                $dailyCalories = $bmr * 1.2;
+                break;
+            case 1:
+                $dailyCalories = $bmr * 1.375;
+                break;
+            case 2:
+                $dailyCalories = $bmr * 1.55;
+                break;
+            case 3:
+                $dailyCalories = $bmr * 1.725;
+                break;
+            case 4:
+                $dailyCalories = $bmr * 1.9;
+                break;
+        }
+
+        $pecahanwaktu = [0, 0, 0];
+        $booleanwaktu = [0, 0, 0];
+        for ($i = 0; $i < 3; $i++) {
+            if (is_array($pecahanwaktu)) {
+                $pecahanwaktu[$i] = \substr($times, $i, 1);
+                if ($pecahanwaktu[$i] == 1) {
+                    $booleanwaktu[0] = 1;
+                } else if ($pecahanwaktu[$i] == 2) {
+                    $booleanwaktu[1] = 1;
+                } else if ($pecahanwaktu[$i] == 3) {
+                    $booleanwaktu[2] = 1;
+                }
+            } else {
+                $booleanwaktu[$times - 1] = 1;
+            }
+        }
+
+        $pecahanhari = [0, 0, 0, 0, 0, 0, 0];
+        $booleanhari = [0, 0, 0, 0, 0, 0, 0];
+
+        for ($i = 0; $i < 7; $i++) {
+            if (is_array($pecahanhari)) {
+                $pecahanhari[$i] = \substr($days, $i, 1);
+                if ($pecahanhari[$i] == 1) {
+                    $booleanhari[0] = 1;
+                } else if ($pecahanhari[$i] == 2) {
+                    $booleanhari[1] = 1;
+                } else if ($pecahanhari[$i] == 3) {
+                    $booleanhari[2] = 1;
+                } else if ($pecahanhari[$i] == 4) {
+                    $booleanhari[3] = 1;
+                } else if ($pecahanhari[$i] == 5) {
+                    $booleanhari[4] = 1;
+                } else if ($pecahanhari[$i] == 6) {
+                    $booleanhari[5] = 1;
+                } else if ($pecahanhari[$i] == 7) {
+                    $booleanhari[6] = 1;
+                }
+            } else {
+                $booleanhari[$days - 1] = 1;
+            }
+        }
+
+        $startHari = 0;
+        $jam = date('H') + 7 % 24;
+        $pengali = 0;
+        if ($jam > 17) {
+            if ($startHari + 1 > 7) {
+                $startHari = 0;
+                $pengali = 1;
+            }
+        }
+        if ($startHari + 1 >= 7) {
+            $startHari = ($hari + 1) == 7 ? 6 : ($hari + 1) % 6;
+            $pengali = 1;
+        } else {
+            $startHari = ($hari + 1) == 7 ? 6 : ($hari + 1) % 6;
+        }
+        $kontrolsekali = true;
+        $bulanPesanan = date('n');
+        $tahunPesanan = date("Y");
+        $date = $date - $hari;
+        $temp = $amount;
+        $check = false;
+        for ($banyak = 0; $banyak < $temp;) {
+            if ($temp != $amount) {
+                $banyak++;
+            }
+            for ($i = $kontrolsekali ? $startHari : 0; $i < 7; $i++) {
+                if ($booleanhari[$i] == 1) {
+                    $tanggalPesanan = $date + $i + ($pengali * 7);
+                    if ($tanggalPesanan > (365 - date('z'))) {
+                        $tahunPesanan += 1;
+                        $bulanPesanan = 1;
+                        $tanggalPesanan -= 365 - date('z');
+                        if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1] + $bulan[$bulanSekarang + 2]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1] + $bulan[$bulanSekarang + 2]);
+                            $bulanPesanan = $bulanSekarang + 4;
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1]);
+                            $bulanPesanan = $bulanSekarang + 3;
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1]);
+                            $bulanPesanan = $bulanSekarang + 2;
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang - 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang - 1]);
+                            $bulanPesanan = $bulanSekarang + 1;
+                        } else {
+                            $bulanPesanan = $bulanSekarang;
+                        }
+                    } else {
+                        if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1] + $bulan[$bulanSekarang + 2]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1] + $bulan[$bulanSekarang + 2]);
+                            $bulanPesanan = $bulanSekarang + 4;
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1] + $bulan[$bulanSekarang + 1]);
+                            $bulanPesanan = $bulanSekarang + 3;
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang] + $bulan[$bulanSekarang - 1]);
+                            $bulanPesanan = $bulanSekarang + 2;
+                        } else if ($tanggalPesanan > $bulan[$bulanSekarang - 1]) {
+                            $tanggalPesanan = $date + $i + ($pengali * 7) - ($bulan[$bulanSekarang - 1]);
+                            $bulanPesanan = $bulanSekarang + 1;
+                        } else {
+                            $bulanPesanan = $bulanSekarang;
+                        }
+                    }
+                }
+                if ($i == 6) {
+                    $kontrolsekali = false;
+                    $pengali++;
+                }
+                for ($j = 1; $j <= 3; $j++) {
+                    if ($amount == 0) {
+                        break;
+                    }
+                    if ($booleanhari[$i] == 1 && $booleanwaktu[$j - 1] == 1) {
+                        if ($amount != 0) {
+                            $dateInput = $tanggalPesanan . "-" . $bulanPesanan . "-" . $tahunPesanan;
+                            $datePesanan = DateTime::createFromFormat('d-m-Y', $dateInput)->format('Y-m-d');
+                            $datenow = date("Y-m-d H:i:s");
+                            $stmt = $this->conn->prepare("INSERT INTO `transactions`(`invoice`, `product_id`, `user_id`, `date`, `notes`, `times`, `proof_of_payment`, `status`, `created_at`, `updated_at`) VALUES(?,?,?,?,?,?,?,?,?,?)");
+                            $status = 1;
+                            $proof = " ";
+                            $temp = $notes;
+                            $notes = "Daily Calorie : ".$dailyCalories."\r\n";
+                            $notes .= $temp;
+                            if ($stmt != FALSE) {
+                                $stmt->bind_param("ssssssssss", $invoice, $product_id, $user_id, $datePesanan, $notes, $j, $proof, $status, $datenow, $datenow);
+                                $amount--;
+                                if ($stmt->execute()) {
+                                    $stmt->close();
+                                    $stmt = $this->conn->prepare("SELECT * FROM transactions WHERE invoice =? LIMIT 1");
+                                    $stmt->bind_param("s", $invoice);
+                                    $stmt->execute();
+                                    $stmt->close();
+                                    $check = true;
+                                } else {
+                                    $response['error'] = true;
+                                    $response['message'] = "Data not inserted";
+                                    echo json_encode($response);
+                                }
+                            } else {
+                                $response['error'] = true;
+                                $response['message'] = "Post Error";
+                                echo json_encode($response);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        $stmt = $this->conn->prepare("SELECT * FROM transactions WHERE invoice=?");
+        if ($stmt != false) {
+            $stmt->bind_param("s", $invoice);
+            if ($stmt->execute()) {
+                $transactions = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+                $stmt->close();
+                if ($transactions != NULL) {
+                    $check = true;
+                } else {
+                    $check = false;
+                }
+            }
+        }
+
+        if ($check) {
+            return $transactions;
+        } else {
+            return false;
+        }
+    }
 
     public function UpdateNotes($uid)
     { }
